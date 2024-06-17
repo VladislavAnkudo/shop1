ff3fd54142b25a30635cafa7df5396fa9fd966c5
VladislavAnkudo <ankudo47@gmail.com>
2024-06-17 22:20:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/normalize.css'
+import './css/style.css'
+import {Route, Routes} from "react-router-dom"
+
+
+import {Authorization} from './pages/authorization';
+import {Redaction} from './pages/redaction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Routes>
+      <Route index path='/' element={<Authorization/>}/>
+      <Route path='redaction' element={<Redaction/>}/>
+    </Routes>
+  </>
+  )
 }
 
 export default App;
